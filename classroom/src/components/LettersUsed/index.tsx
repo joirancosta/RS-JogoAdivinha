7a9790e5299bda0c160e5a006b65838f85de0eb1
“joirancosta” <joiran.costa.jc@gmail.com>
2025-07-24 20:01:01 -0300

    config palpite

--- classroom/src/components/LettersUsed/index.tsx
+++ classroom/src/components/LettersUsed/index.tsx
@@ -2,13 +2,25 @@ import styles from "./styles.module.css"
 
 import { Letter } from "../Letter"
 
-export function LettersUsed(){
+export type LettersUsedProps = {
+  value: string
+  correct: boolean
+}
+
+type Props = {
+  data: LettersUsedProps[]
+}
+
+export function LettersUsed({ data }: Props){
   return (
     <div className={styles.lettersUsed} >
       <h5>Letras utilizadas</h5>
-      <div>        
-        <Letter value="E" size="small" color="correct" />
-        <Letter value="X" size="small" color="wrong" />
+      <div>
+        {
+          data.map(({ value, correct }) => (
+            <Letter value={value} size="small" color={correct ? "correct" : "wrong"} />
+          ))
+        }
       </div>
     </div>
   )
